6f45654546bfdd9fe9e5c0d6e3a11675c623d0c7
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-05-18 14:28:52 +0300

    added splash screen

--- app/src/main/java/com/yuriishcherbyna/newssho/presentation/MainActivity.kt
+++ app/src/main/java/com/yuriishcherbyna/newssho/presentation/MainActivity.kt
@@ -7,12 +7,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yuriishcherbyna.newssho.presentation.ui.theme.NewsShoTheme
 import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             NewsShoTheme {
                 Surface(
