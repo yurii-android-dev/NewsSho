f1a9e73672fe2dd5d7990210635b723f62ea3b83
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-05-29 08:57:54 +0300

    provide dependencies for authRepository

---
+++ app/src/main/java/com/yuriishcherbyna/newssho/di/AppModule.kt
@@ -0,0 +1,31 @@
+package com.yuriishcherbyna.newssho.di
+
+import android.content.Context
+import androidx.credentials.CredentialManager
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideCredentialManager(
+        @ApplicationContext context: Context
+    ): CredentialManager {
+        return CredentialManager.create(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth = Firebase.auth
+
+}
