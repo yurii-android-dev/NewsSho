3da90efbb89770b1d38eb5838d03d1f208ad0345
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-06-05 10:50:14 +0300

    created NewsApi

---
+++ app/src/main/java/com/yuriishcherbyna/newssho/data/remote/NewsApi.kt
@@ -0,0 +1,24 @@
+package com.yuriishcherbyna.newssho.data.remote
+
+import com.yuriishcherbyna.newssho.BuildConfig
+import com.yuriishcherbyna.newssho.data.remote.dto.Category
+import com.yuriishcherbyna.newssho.data.remote.dto.NewsDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("top-headlines")
+    suspend fun getLatestNews(
+        @Query("apiKey") apiKey: String = BuildConfig.NEWS_API_KEY,
+        @Query("country") country: String,
+        @Query("category") category: Category? = null
+    ): NewsDto
+
+    @GET("everything")
+    suspend fun searchNews(
+        @Query("apiKey") apiKey: String = BuildConfig.NEWS_API_KEY,
+        @Query("q") query: String,
+    ): NewsDto
+
+}
