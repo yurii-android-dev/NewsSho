c8b7b42054eed1a10a36e84c05a77414b78b8b0c
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-06-06 10:26:18 +0300

    updated NewsRepository

--- app/src/main/java/com/yuriishcherbyna/newssho/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/yuriishcherbyna/newssho/domain/repository/NewsRepository.kt
@@ -1,15 +1,15 @@
 package com.yuriishcherbyna.newssho.domain.repository
 
 import com.yuriishcherbyna.newssho.data.remote.dto.Category
-import com.yuriishcherbyna.newssho.domain.model.NewsItem
+import com.yuriishcherbyna.newssho.data.remote.dto.NewsItemDto
 import com.yuriishcherbyna.newssho.domain.util.DataError
 import com.yuriishcherbyna.newssho.domain.util.Result
 import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
 
-    fun getLatestNews(category: Category): Flow<Result<List<NewsItem>, DataError.Network>>
+    fun getLatestNews(category: Category): Flow<Result<List<NewsItemDto>, DataError.Network>>
 
-    fun searchNews(query: String): Flow<Result<List<NewsItem>, DataError.Network>>
+    fun searchNews(query: String): Flow<Result<List<NewsItemDto>, DataError.Network>>
 
 }
