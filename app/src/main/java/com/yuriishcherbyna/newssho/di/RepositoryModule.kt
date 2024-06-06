743debc869ed7c5256500f62e4b30fb6cc633903
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-06-06 10:58:30 +0300

    binds NewsRepository

--- app/src/main/java/com/yuriishcherbyna/newssho/di/RepositoryModule.kt
+++ app/src/main/java/com/yuriishcherbyna/newssho/di/RepositoryModule.kt
@@ -2,8 +2,10 @@ package com.yuriishcherbyna.newssho.di
 
 import com.yuriishcherbyna.newssho.data.repository.DataStoreRepositoryImpl
 import com.yuriishcherbyna.newssho.data.repository.GoogleAuthRepositoryImpl
+import com.yuriishcherbyna.newssho.data.repository.NewsRepositoryImpl
 import com.yuriishcherbyna.newssho.domain.repository.AuthRepository
 import com.yuriishcherbyna.newssho.domain.repository.DataStoreRepository
+import com.yuriishcherbyna.newssho.domain.repository.NewsRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -25,5 +27,11 @@ abstract class RepositoryModule {
     abstract fun bindAuthRepository(
         googleAuthRepositoryImpl: GoogleAuthRepositoryImpl
     ): AuthRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindNewsRepository(
+        newsRepositoryImpl: NewsRepositoryImpl
+    ): NewsRepository
 
 }
