3da99a68468e57e432ea585bfbe209ecb8457d50
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-06-17 10:54:54 +0300

    updated HomeViewModel

--- app/src/main/java/com/yuriishcherbyna/newssho/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/yuriishcherbyna/newssho/presentation/home/HomeViewModel.kt
@@ -1,11 +1,13 @@
 package com.yuriishcherbyna.newssho.presentation.home
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yuriishcherbyna.newssho.data.remote.dto.Category
 import com.yuriishcherbyna.newssho.domain.repository.NewsRepository
 import com.yuriishcherbyna.newssho.domain.util.Result
 import com.yuriishcherbyna.newssho.presentation.util.toNetworkErrorMessageId
+import com.yuriishcherbyna.newssho.util.Constants.SELECTED_CATEGORY_KEY
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -14,17 +16,24 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val newsRepository: NewsRepository
+    private val newsRepository: NewsRepository,
+    private val savedStateHandle: SavedStateHandle
 ): ViewModel() {
 
     private val _uiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
     val uiState = _uiState.asStateFlow()
 
+    val selectedCategory = savedStateHandle.getStateFlow(
+        SELECTED_CATEGORY_KEY,
+        Category.GENERAL
+    )
+
     init {
-        getLatestNews(Category.GENERAL)
+        getLatestNews(selectedCategory.value)
     }
 
     fun getLatestNews(category: Category) {
+        _uiState.value = HomeUiState.Loading
         viewModelScope.launch {
             newsRepository.getLatestNews(category).collect { result ->
                 when (result) {
@@ -40,6 +49,10 @@ class HomeViewModel @Inject constructor(
                 }
             }
         }
+    }
+
+    fun onCategoryChanged(category: Category) {
+        savedStateHandle["selected_category"] = category
     }
 
 }
