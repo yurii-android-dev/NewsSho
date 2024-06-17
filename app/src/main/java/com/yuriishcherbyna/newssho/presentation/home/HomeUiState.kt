0d2b79b33c30f280de4c8964b07dea4e007cd4b3
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-06-17 12:49:05 +0300

    updated HomeUiState

--- app/src/main/java/com/yuriishcherbyna/newssho/presentation/home/HomeUiState.kt
+++ app/src/main/java/com/yuriishcherbyna/newssho/presentation/home/HomeUiState.kt
@@ -3,8 +3,8 @@ package com.yuriishcherbyna.newssho.presentation.home
 import androidx.annotation.StringRes
 import com.yuriishcherbyna.newssho.domain.model.NewsItem
 
-sealed interface HomeUiState {
-    data class Success(val news: List<NewsItem>): HomeUiState
-    data object Loading: HomeUiState
-    data class Error(@StringRes val error: Int): HomeUiState
-}
+data class HomeUiState(
+    val news: List<NewsItem> = emptyList(),
+    val isLoading: Boolean = false,
+    @StringRes val errorMessage: Int? = null
+)
