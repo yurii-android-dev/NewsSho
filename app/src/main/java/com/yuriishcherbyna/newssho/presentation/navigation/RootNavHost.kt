64e40a8d0d81f938043998239bebef372899ff89
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-06-28 10:23:15 +0300

    implemented bottom navigation bar

--- app/src/main/java/com/yuriishcherbyna/newssho/presentation/navigation/RootNavHost.kt
+++ app/src/main/java/com/yuriishcherbyna/newssho/presentation/navigation/RootNavHost.kt
@@ -1,15 +1,30 @@
 package com.yuriishcherbyna.newssho.presentation.navigation
 
+import android.annotation.SuppressLint
 import android.widget.Toast
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.yuriishcherbyna.newssho.R
@@ -24,110 +39,170 @@ import com.yuriishcherbyna.newssho.util.Constants.DETAILS_SCREEN_TITLE_ARG
 import com.yuriishcherbyna.newssho.util.Constants.DETAILS_SCREEN_URL_ARG
 import java.net.URLEncoder
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun RootNavHost(
     startDestination: String
 ) {
 
     val navController = rememberNavController()
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+    
+    var selectedItem by remember { mutableIntStateOf(0) }
+    val showBottomBar by remember {
+        derivedStateOf {
+            currentBackStackEntry?.destination?.hierarchy?.
+                any {
+                    it.route == Screens.Welcome.route ||
+                    it.route == Screens.SignIn.route ||
+                    it.route == Screens.Details.route
+                } == false
+        }
+    }
+    val items = listOf(
+        Screens.Home,
+        Screens.Bookmarks,
+        Screens.Profile
+    )
 
-    NavHost(
-        navController = navController,
-        startDestination = startDestination
+    Scaffold(
+        bottomBar = {
+            if (showBottomBar) {
+                NavigationBar {
+                    items.forEachIndexed { index, screen ->
+                        NavigationBarItem(
+                            icon = {
+                                Icon(
+                                    painter = painterResource(id = screen.icon),
+                                    contentDescription = stringResource(id = screen.label)
+                                )
+                            },
+                            label = { Text(text = stringResource(id = screen.label)) },
+                            selected = selectedItem == index,
+                            onClick = {
+                                selectedItem = index
+                                navController.navigate(screen.route) {
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = true
+                                    }
+                                    launchSingleTop = true
+                                    restoreState = true
+                                }
+                            },
+                        )
+                    }
+                }
+            }
+        }
     ) {
-        composable(
-            route = Screens.Welcome.route
+        NavHost(
+            navController = navController,
+            startDestination = startDestination
         ) {
+            composable(
+                route = Screens.Welcome.route
+            ) {
 
-            val welcomeViewModel: WelcomeViewModel = hiltViewModel()
+                val welcomeViewModel: WelcomeViewModel = hiltViewModel()
 
-            WelcomeScreen(
-                onDoneClicked = {
-                    welcomeViewModel.saveOnboardingState(true)
-                    navController.navigate(Screens.SignIn.route) {
-                        popUpTo(navController.graph.startDestinationId) {
-                            inclusive = true
+                WelcomeScreen(
+                    onDoneClicked = {
+                        welcomeViewModel.saveOnboardingState(true)
+                        navController.navigate(Screens.SignIn.route) {
+                            popUpTo(navController.graph.startDestinationId) {
+                                inclusive = true
+                            }
                         }
                     }
-                }
-            )
-        }
-        composable(
-            route = Screens.SignIn.route,
-        ) {
-
-            val signInViewModel: SignInViewModel = hiltViewModel()
-            val uiState by signInViewModel.uiState.collectAsState()
-
-            val context = LocalContext.current
-
-            LaunchedEffect(key1 = Unit) {
-                if (signInViewModel.getSignedInUser() != null) {
-                    navController.navigate(Screens.Home.route) {
-                        popUpTo(Screens.SignIn.route) {
-                            inclusive = true
-                        }
-                    }
-                }
+                )
             }
+            composable(
+                route = Screens.SignIn.route,
+            ) {
 
-            LaunchedEffect(key1 = uiState.isLoggedIn) {
-                if (uiState.isLoggedIn) {
-                    Toast.makeText(
-                        context,
-                        context.getString(R.string.sign_in_successfully),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    navController.navigate(Screens.Home.route) {
-                        popUpTo(Screens.SignIn.route) {
-                            inclusive = true
+                val signInViewModel: SignInViewModel = hiltViewModel()
+                val uiState by signInViewModel.uiState.collectAsState()
+
+                val context = LocalContext.current
+
+                LaunchedEffect(key1 = Unit) {
+                    if (signInViewModel.getSignedInUser() != null) {
+                        navController.navigate(Screens.Home.route) {
+                            popUpTo(Screens.SignIn.route) {
+                                inclusive = true
+                            }
                         }
                     }
-                    signInViewModel.resetState()
                 }
+
+                LaunchedEffect(key1 = uiState.isLoggedIn) {
+                    if (uiState.isLoggedIn) {
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.sign_in_successfully),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        navController.navigate(Screens.Home.route) {
+                            popUpTo(Screens.SignIn.route) {
+                                inclusive = true
+                            }
+                        }
+                        signInViewModel.resetState()
+                    }
+                }
+
+                SignInScreen(
+                    uiState = uiState,
+                    onSignInClick = {
+                        signInViewModel.signInWithGoogle()
+                    }
+                )
             }
+            composable(
+                route = Screens.Home.route
+            ) {
 
-            SignInScreen(
-                uiState = uiState,
-                onSignInClick = {
-                    signInViewModel.signInWithGoogle()
-                }
-            )
-        }
-        composable(
-            route = Screens.Home.route
-        ) {
+                val homeViewModel: HomeViewModel = hiltViewModel()
+                val homeUiState by homeViewModel.uiState.collectAsState()
+                val selectedCategory by homeViewModel.selectedCategory.collectAsState()
 
-            val homeViewModel: HomeViewModel = hiltViewModel()
-            val homeUiState by homeViewModel.uiState.collectAsState()
-            val selectedCategory by homeViewModel.selectedCategory.collectAsState()
+                HomeScreen(
+                    uiState = homeUiState,
+                    selectedCategory = selectedCategory,
+                    onAction = homeViewModel::onAction,
+                    onNewsClicked = { url, title ->
+                        val encodedUrl = URLEncoder.encode(url, "UTF-8")
+                        navController.navigate(Screens.Details.passUrlAndTitle(encodedUrl, title))
+                    }
+                )
+            }
+            composable(
+                route = Screens.Details.route,
+                arguments = listOf(
+                    navArgument(DETAILS_SCREEN_URL_ARG) { type = NavType.StringType },
+                    navArgument(DETAILS_SCREEN_TITLE_ARG) { type = NavType.StringType }
+                )
+            ) {
 
-            HomeScreen(
-                uiState = homeUiState,
-                selectedCategory = selectedCategory,
-                onAction = homeViewModel::onAction,
-                onNewsClicked = { url, title ->
-                    val encodedUrl = URLEncoder.encode(url, "UTF-8")
-                    navController.navigate(Screens.Details.passUrlAndTitle(encodedUrl, title))
-                }
-            )
-        }
-        composable(
-            route = Screens.Details.route,
-            arguments = listOf(
-                navArgument(DETAILS_SCREEN_URL_ARG) { type = NavType.StringType },
-                navArgument(DETAILS_SCREEN_TITLE_ARG) { type = NavType.StringType }
-            )
-        ) {
+                val url = it.arguments?.getString(DETAILS_SCREEN_URL_ARG)
+                val title = it.arguments?.getString(DETAILS_SCREEN_TITLE_ARG)
 
-            val url = it.arguments?.getString(DETAILS_SCREEN_URL_ARG)
-            val title = it.arguments?.getString(DETAILS_SCREEN_TITLE_ARG)
-
-            DetailsScreen(
-                url = url,
-                title = title,
-                onNavigateBackClicked = { navController.navigateUp() }
-            )
+                DetailsScreen(
+                    url = url,
+                    title = title,
+                    onNavigateBackClicked = { navController.navigateUp() }
+                )
+            }
+            composable(
+                route = Screens.Bookmarks.route
+            ) {
+                Text(text = "Bookmarks")
+            }
+            composable(
+                route = Screens.Profile.route
+            ) {
+                Text(text = "Profile")
+            }
         }
     }
 
--- app/src/main/java/com/yuriishcherbyna/newssho/presentation/navigation/Screens.kt
+++ app/src/main/java/com/yuriishcherbyna/newssho/presentation/navigation/Screens.kt
@@ -1,18 +1,53 @@
 package com.yuriishcherbyna.newssho.presentation.navigation
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.yuriishcherbyna.newssho.R
 import com.yuriishcherbyna.newssho.util.Constants.DETAILS_SCREEN_TITLE_ARG
 import com.yuriishcherbyna.newssho.util.Constants.DETAILS_SCREEN_URL_ARG
 
-sealed class Screens(val route: String) {
+sealed class Screens(
+    val route: String,
+    @StringRes val label: Int,
+    @DrawableRes val icon: Int
+) {
 
-    data object Welcome : Screens("welcome_screen")
-    data object SignIn : Screens("sign_in_screen")
+    data object Welcome : Screens(
+        route = "welcome_screen",
+        label = -1,
+        icon = -1
+    )
+    data object SignIn : Screens(
+        route = "sign_in_screen",
+        label = -1,
+        icon = -1
+    )
 
-    data object Home : Screens("home_screen")
+    data object Home : Screens(
+        route = "home_screen",
+        label = R.string.home_label,
+        icon = R.drawable.ic_home_24
+    )
 
     data object Details :
-        Screens("details_screen/{$DETAILS_SCREEN_URL_ARG}/{$DETAILS_SCREEN_TITLE_ARG}") {
+        Screens(
+            route = "details_screen/{$DETAILS_SCREEN_URL_ARG}/{$DETAILS_SCREEN_TITLE_ARG}",
+            label = -1,
+            icon = -1
+        ) {
         fun passUrlAndTitle(url: String, title: String) = "details_screen/$url/$title"
     }
+
+    data object Bookmarks : Screens(
+        route = "bookmarks_screen",
+        label = R.string.bookmarks_label,
+        icon = R.drawable.ic_bookmark_24
+    )
+
+    data object Profile : Screens(
+        route = "profile_screen",
+        label = R.string.profile_label,
+        icon = R.drawable.ic_account_circle_24
+    )
 
 }
